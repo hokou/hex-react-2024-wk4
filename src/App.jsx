87246e2ecb06a036be2d83c0d3b5d29dfb451c1a
hokou <hokou.shiau@gmail.com>
2025-03-02 23:56:34 +0800

    add Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState, useRef } from "react"
 import axios from "axios"
 import { Modal } from "bootstrap";
+import LoginPage from "./pages/LoginPage";
+
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 const API_PATH = import.meta.env.VITE_API_PATH;
 
@@ -22,21 +24,7 @@ function App() {
 
   const [products, setProducts] = useState([]);
   
-  const [account, setAccount] = useState({
-    username: "example@test.com",
-    password: "example"
-  })
-
   const [pageInfo, setPageInfo] = useState({});
-
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target
-    setAccount({
-      ...account,
-      [name]: value
-    })
-  }
 
   const getProducts = async (page = 1) => {
     try {
@@ -47,26 +35,6 @@ function App() {
       setPageInfo(res.data.pagination);
     } catch (error) {
       alert("取得產品失敗");
-      console.error(error);
-    }
-  };
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
-    try {
-      const res = await axios.post(`${BASE_URL}/v2/admin/signin`, account);
-
-      const { token, expired } = res.data;
-      document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
-
-      axios.defaults.headers.common["Authorization"] = token;
-
-      getProducts();
-
-      setIsAuth(true);
-    } catch (error) {
-      alert("登入失敗");
       console.error(error);
     }
   };
@@ -321,22 +289,7 @@ function App() {
         </div>
       </div>
     </div>
-    ) : <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-        <h1 className="mb-5">請先登入</h1>
-        <form onSubmit={handleLogin} className="d-flex flex-column gap-3">
-          <div className="form-floating mb-3">
-            <input name="username" value={account.username} onChange={handleInputChange} type="email" className="form-control" id="username" placeholder="name@example.com" />
-            <label htmlFor="username">Email address</label>
-          </div>
-          <div className="form-floating">
-            <input name="password" value={account.password} onChange={handleInputChange}  type="password" className="form-control" id="password" placeholder="Password" />
-            <label htmlFor="password">Password</label>
-          </div>
-          <button type="submit" className="btn btn-primary">登入</button>
-        </form>
-        <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
-      </div>
-    }
+    ) : <LoginPage getProducts={getProducts}/>}
 
       <div ref={productRef} id="productModal" className="modal" style={{ backgroundColor: "rgba(0,0,0,0.5)" }}>
         <div className="modal-dialog modal-dialog-centered modal-xl">
